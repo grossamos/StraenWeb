9caecb773c68d27101e322f78c80c660409e7851
Mike Simms <msimms01@gmail.com>
2014-04-30 20:10:17 -0400

    Now receives data.

--- workouts.py
+++ workouts.py
@@ -6,46 +6,58 @@ import threading
 
 class Database(object):
 	def __init__(self):
-		self.con = sqlite3.connect('workouts.db')
+		super(Database, self).__init__()
 
 	def create(self):
-		with self.con:
-			self.cur = self.con.cursor()
+		con = sqlite3.connect('workouts.db')
+		with con:
+			self.cur = con.cursor()
 			self.cur.execute("create table location (device text primary key, activityId integer, latitude double, longitude double, altitude double)")
 
 	def storeLocation(self, device, activityId, latitude, longitude, altitude):
-		with self.con:
+		con = sqlite3.connect('workouts.db')
+		with con:
 			sql = "insert into location values(" + device + ", " + str(activityId) + ", " + str(latitude) + ", " + str(longitude) + ", " + str(altitude) + ")"
 			self.cur.execute(sql)
+
+	def listLocations(self, device, activityId):
+		locations = []
+
+		con = sqlite3.connect('workouts.db')
+		with con:
+			sql = "select latitude, longitude, altitude from location where device = '" + device + "' and activityId = " + str(activityId)
+			self.cur.execute(sql)
+
+		return locations
 
 class DataListener(threading.Thread):
 	def __init__(self, db):
 		self.db = db
+		self.stop = threading.Event()
+		super(DataListener, self).__init__()
+
+	def terminate():
+		self.stop.set()
 
 	def parseJsonStr(self, str):
-		pass
+		decoder = json.JSONDecoder()
+		print json.dumps(str)
 
 	def readLine(self, sock):
-		line = ""
-
-		while True:
-			d = sock.recvfrom(1)
-			data = d[0]
-			addr = d[1]
-
-			if not data:
-				break
-
-		return line
+		while not self.stop.is_set():
+			data, addr = sock.recvfrom(1024)
+			return data
+		
+		return ""
 
 	def run(self):
-		UDP_IP = "127.0.0.1"
+		UDP_IP = ""
 		UDP_PORT = 5150
 
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		sock.bind((UDP_IP, UDP_PORT))
 
-		while True:
+		while not self.stop.is_set():
 			line = self.readLine(sock)
 			if line:
 				self.parseJsonStr(line)
@@ -53,12 +65,19 @@ class DataListener(threading.Thread):
 class DataMgr(object):
 	def __init__(self):
 		self.db = Database()
-		self.listener = DataListener()
+		self.listener = DataListener(self.db)
 		self.listener.start()
+		super(DataMgr, self).__init__()
 
 class FollowMyWorkout(object):
+	def __init__(self, mgr):
+		self.mgr = mgr
+		super(FollowMyWorkout, self).__init__()
+	
 	def index(self):
-		return """
+		locations = self.mgr.db.listLocations("", 0)
+
+		html = """
 
 <!DOCTYPE html>
 <html>
@@ -81,13 +100,27 @@ class FollowMyWorkout(object):
 		{
 			var mapOptions =
 			{
-				center: new google.maps.LatLng(-34.397, 150.644),
+"""
+
+		if len(locations) > 0:
+			html += "center: new google.maps.LatLng(" + str(locations[0].latitude) + ", " + str(locations[0].longitude) + "),\n"
+		else:
+			html += "center: new google.maps.LatLng(0.0, 0.0),\n"
+
+		html += """
 				zoom: 10
 			};
 			var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
 			var flightPlanCoordinates =
 			[
+"""
+
+		for location in locations:
+			str = "new google.maps.LatLng(" + str(location.latitude) + ", " + str(location.longitude) + "),\n"
+			html += str
+
+		html += """
 				new google.maps.LatLng(37.772323, -122.214897),
 				new google.maps.LatLng(21.291982, -157.821856),
 				new google.maps.LatLng(-18.142599, 178.431),
@@ -117,8 +150,9 @@ class FollowMyWorkout(object):
 </html>
 
 """
+		return html
 
 	index.exposed = True
 
 mgr = DataMgr()
-cherrypy.quickstart(FollowMyWorkout())
+cherrypy.quickstart(FollowMyWorkout(mgr))
