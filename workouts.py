c6cd0dabd339f2bbd74ac3bb2c9826459af2e0e2
Mike Simms <msimms01@gmail.com>
2014-08-16 11:40:26 -0400

    Started adding authentication.

--- workouts.py
+++ workouts.py
@@ -16,6 +16,34 @@ from cherrypy import tools
 from mako.lookup import TemplateLookup
 from mako.template import Template
 
+SESSION_KEY = '_cp_username'
+
+def check_auth(*args, **kwargs):
+	# A tool that looks in config for 'auth.require'. If found and it is not None, a login
+	# is required and the entry is evaluated as a list of conditions that the user must fulfill
+	conditions = cherrypy.request.config.get('auth.require', None)
+	if conditions is not None:
+		username = cherrypy.session.get(SESSION_KEY)
+		if username:
+			cherrypy.request.login = username
+			for condition in conditions:
+				# A condition is just a callable that returns true or false
+				if not condition():
+					raise cherrypy.HTTPRedirect("/login")
+		else:
+			raise cherrypy.HTTPRedirect("/login")
+
+def require(*conditions):
+	# A decorator that appends conditions to the auth.require config variable.
+	def decorate(f):
+		if not hasattr(f, '_cp_config'):
+			f._cp_config = dict()
+		if 'auth.require' not in f._cp_config:
+			f._cp_config['auth.require'] = []
+		f._cp_config['auth.require'].extend(conditions)
+		return f
+	return decorate
+
 class Location(object):
 	def __init__(self):
 		self.latitude = 0.0
@@ -270,6 +298,11 @@ class DataMgr(object):
 		return self.db.storeUser(username, firstname, lastname, hash)
 
 class WorkoutsWeb(object):
+	_cp_config = {
+		'tools.sessions.on': True,
+		'tools.auth.on': True
+	}
+
 	def __init__(self, mgr):
 		self.mgr = mgr
 		super(WorkoutsWeb, self).__init__()
@@ -386,6 +419,11 @@ class WorkoutsWeb(object):
 			deviceId = self.mgr.db.getDeviceId(deviceStr)
 			activityId = self.mgr.db.getLatestActivityId(deviceId)
 			locations = self.mgr.db.listLocations(deviceId, activityId)
+
+			if len(locations) == 0:
+				myTemplate = Template(filename='auth_error.html', module_directory='tempmod')
+				return myTemplate.render(error="There is no data for the specified user.")
+				
 			route = ""
 			centerLat = 0
 			centerLon = 0
@@ -397,7 +435,7 @@ class WorkoutsWeb(object):
 				centerLat = locations[0].latitude
 				centerLon = locations[0].longitude
 
-			myTemplate = Template(filename='map.html', module_directory='tempmod')
+			myTemplate = Template(filename='map_single.html', module_directory='tempmod')
 			return myTemplate.render(deviceStr=deviceStr, centerLat=centerLat, centerLon=centerLon, route=route, routeLen=len(locations), activityId=str(activityId))
 		except:
 			cherrypy.response.status = 500
@@ -410,8 +448,12 @@ class WorkoutsWeb(object):
 	def login_submit(self, username, password, *args, **kw):
 		try:
 			if self.mgr.authenticateUser(username, password):
-				myTemplate = Template(filename='error.html', module_directory='tempmod')
-				return myTemplate.render(error="Logged in")
+				cherrypy.session.regenerate()
+				cherrypy.session[SESSION_KEY] = cherrypy.request.login = username
+				self.on_login(username)
+
+				myTemplate = Template(filename='dashboard.html', module_directory='tempmod')
+				return myTemplate.render()
 			else:
 				myTemplate = Template(filename='error.html', module_directory='tempmod')
 				return myTemplate.render(error="Unable to authenticate the user")
@@ -452,8 +494,9 @@ class WorkoutsWeb(object):
 		return myTemplate.render()
 
 	@cherrypy.expose
-	def followers(self):
-		myTemplate = Template(filename='followers.html', module_directory='tempmod')
+	@require()
+	def dashboard(self, username=None, *args, **kw):
+		myTemplate = Template(filename='dashboard.html', module_directory='tempmod')
 		return myTemplate.render()
 
 	@cherrypy.expose
@@ -492,5 +535,6 @@ conf = {
 }
 
 cherrypy.config.update( {'server.socket_host': '0.0.0.0'} )
+cherrypy.tools.auth = cherrypy.Tool('before_handler', check_auth)
 cherrypy.engine.signals.subscribe()
 cherrypy.quickstart(WorkoutsWeb(mgr), config=conf)
