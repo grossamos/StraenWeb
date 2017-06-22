4a863a7bcc76c2cfcc57c151a4acb1c58b91c00f
Mike Simms <msimms01@gmail.com>
2017-06-22 15:24:43 -0400

    Changed default protocol.

--- DataListener.py
+++ DataListener.py
@@ -237,7 +237,7 @@ if __name__ == "__main__":
 	# Parse command line options.
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--rootdir", type=str, action="store", default=os.path.dirname(os.path.realpath(__file__)), help="Directory for database and logs", required=False)
-	parser.add_argument("--protocol", type=str, action="store", default="udp", help="udp|rest", required=False)
+	parser.add_argument("--protocol", type=str, action="store", default="rest", help="udp|rest", required=False)
 	parser.add_argument("--port", type=int, action="store", default=8081, help="Port on which to listen", required=False)
 	parser.add_argument("--debug", action="store_true", default=False, help="", required=False)
 	parser.add_argument("--datalog", action="store_true", default=False, help="", required=False)
